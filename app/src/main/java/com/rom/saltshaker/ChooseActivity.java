b7652bc0274d8f1c1bab3f73f228273e6acd65d3
jiyujung <s2019w39@e-mirim.hs.kr>
2020-10-28 00:43:37 +0900

    first commit

---
+++ app/src/main/java/com/rom/saltshaker/ChooseActivity.java
@@ -0,0 +1,39 @@
+package com.rom.saltshaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class ChooseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose);
+
+        ImageButton homeActivity = findViewById(R.id.homeActivity);
+        ImageButton memory = findViewById(R.id.memory);
+
+        homeActivity.setOnClickListener(imgBtnHandler);
+        memory.setOnClickListener(imgBtnHandler);
+    }
+
+    View.OnClickListener imgBtnHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = null;
+            switch (v.getId()) {
+                case R.id.homeActivity:
+                    intent = new Intent(getApplicationContext(), HomeActivity.class);
+                    break;
+                case R.id.memory:
+                    break;
+            }
+            startActivity(intent);
+        }
+    };
+}
