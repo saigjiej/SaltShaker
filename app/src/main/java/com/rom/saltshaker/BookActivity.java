7b8c57145bddbb7fffe411d8374f3a6777a706b1
jiyujung <s2019w39@e-mirim.hs.kr>
2020-11-09 23:53:31 +0900

    complete Book

--- app/src/main/java/com/rom/saltshaker/BookActivity.java
+++ app/src/main/java/com/rom/saltshaker/BookActivity.java
@@ -1,14 +1,29 @@
 package com.rom.saltshaker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class BookActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Book> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,24 +32,37 @@ public class BookActivity extends AppCompatActivity {
 
         //툴바 사용
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-        //툴바 타이틀 지우기
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        //툴바 뒤로가기 생성(뒤로가기 화살표가 자동 제공됨)
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //뒤로가기 버튼 설정(drawable에 이미지를 등록해서 화살표 대신 사용가능, 사이즈가 충분히 작아야 함)
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back);
-        //툴바 배경색
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#C6D6F7")));
+
+        recyclerView = findViewById(R.id.recyclerView2); // 아이디 연결
+        recyclerView.setHasFixedSize(true); // 리사이클러뷰 기존성능 강화
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>();  // User 객체를 담을 어레이 리스트 (어댑터쪽으로)
+
+        database = FirebaseDatabase.getInstance();  // 파이어베이스 데이터베이스 연동
+
+        databaseReference = database.getReference("Book");  // DB 테이블 연결
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                arrayList.clear();  // 기존 배열리스트가 존재하지 않게 초기화
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {  // 반복문으로 데이터 List를 추출해냄
+                    Book book = snapshot.getValue(Book.class);  // 만들어뒀던 User 객체에 데이터를 담는다.
+                    arrayList.add(book);    // 담은 데이터들을 배열리스트에 넣고 리사일클러뷰로 보낼 준비
+                }
+                adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // 디비를 가져오는 중 에러 발생 시
+                Log.e("BookActivity", String.valueOf(databaseError.toException()));    // 에러문 출력
+            }
+        });
+
+        adapter = new BookCustomAdapter(arrayList, this);
+        recyclerView.setAdapter(adapter);   // 리사이클러뷰에 어댑터 연결
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:{ //툴바 뒤로가기 동작
-                finish();
-                return true;
-            }
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
