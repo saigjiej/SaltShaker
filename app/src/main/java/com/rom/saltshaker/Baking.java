50d222d5cc1c3dfcb50501fe34a6a294966bc3c7
jiyujung <s2019w39@e-mirim.hs.kr>
2020-11-10 23:02:49 +0900

    Complete Baking

---
+++ app/src/main/java/com/rom/saltshaker/Baking.java
@@ -0,0 +1,89 @@
+package com.rom.saltshaker;
+
+public class Baking {
+    private String poster;
+    private String how;
+    private String title;
+    private String material;
+    private String making1;
+    private String making2;
+    private String making3;
+    private String making4;
+    private String materialText;
+
+    public Baking() {
+
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getHow() {
+        return how;
+    }
+
+    public void setHow(String how) {
+        this.how = how;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getMaking1() {
+        return making1;
+    }
+
+    public void setMaking1(String making1) {
+        this.making1 = making1;
+    }
+
+    public String getMaking2() {
+        return making2;
+    }
+
+    public void setMaking2(String making2) {
+        this.making2 = making2;
+    }
+
+    public String getMaking3() {
+        return making3;
+    }
+
+    public void setMaking3(String making3) {
+        this.making3 = making3;
+    }
+
+    public String getMaking4() {
+        return making4;
+    }
+
+    public void setMaking4(String making4) {
+        this.making4 = making4;
+    }
+
+    public String getMaterialText() {
+        return materialText;
+    }
+
+    public void setMaterialText(String materialText) {
+        this.materialText = materialText;
+    }
+}
